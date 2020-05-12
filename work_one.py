6174941ccea86083464aa3f05b98ef689780e9d4
AlexandraGty <52900628+AlexandraGty@users.noreply.github.com>
2020-05-12 20:19:51 +0800

    第二次修改提交
    增加了异常处理

--- work_one.py
+++ work_one.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
 """
   Author:  Ty.Gu
   Purpose: Generate random data set.
@@ -10,57 +10,74 @@ import string
 
 def create_set(datatype, datarange, num, strlen=8):
     old_set = set()
-    for i in range(0,num):
+    try:
         if datatype is int:
-            it = iter(datarange)
-            item = random.randint(next(it), next(it))
-            old_set.add(item)
-            continue
+            for i in range(0, num):
+                it = iter(datarange)
+                item = random.randint(next(it), next(it))
+                old_set.add(item)
+                continue
 
         elif datatype is float:
-            it = iter(datarange)
-            item = random.uniform(next(it), next(it))
-            old_set.add(item)
-            continue
+            for i in range(0, num):
+                it = iter(datarange)
+                item = random.uniform(next(it), next(it))
+                old_set.add(item)
+                continue
 
         elif datatype is str:
-            item = ''.join(random.SystemRandom().choice(datarange) for _ in range(strlen))
-            old_set.add(item)
-            continue
-        else:
-            continue
-    return old_set
+            for i in range(0, num):
+                item = ''.join(random.SystemRandom().choice(datarange) for _ in range(strlen))
+                old_set.add(item)
+                continue
+        return old_set
+    except Exception as e:
+        print(e)
+        print('你输入的参数有误')
 
 
-def select_set(old_set,datatype, datarange):
+
+def select_set(old_set, datatype, datarange):
     new_set = set()
-    for x in old_set:
-        if datatype is int:
-            it = iter(datarange)
-            if next(it) <= x <= next(it):
-                new_set.add(x)
-            continue
+    try:
+        for x in old_set:
+            if datatype is int:
+                it = iter(datarange)
+                if next(it) <= x <= next(it):
+                    new_set.add(x)
+                continue
 
-        elif datatype is float:
-            it = iter(datarange)
-            if next(it) <= x <= next(it):
-                new_set.add(x)
-            continue
+            elif datatype is float:
+                it = iter(datarange)
+                if next(it) <= x <= next(it):
+                    new_set.add(x)
+                continue
 
-        elif datatype is str:
-            if x.find(datarange) != -1:
-                new_set.add(x)
-            continue
-        else:
-            continue
-    return new_set
-
+            elif datatype is str:
+                if x.find(datarange) != -1:
+                    new_set.add(x)
+                continue
+        return new_set
+    except Exception as e:
+        print(e)
+        print('你输入的参数有误')
 
 def apply():
-    base_str = string.ascii_letters + string.digits + string.punctuation
-    old_set = create_set(str, base_str, 10)
-    print(old_set)
-    new_set = select_set(old_set,str,'at')
-    print(new_set)
+    base_str = string.ascii_letters + string.digits #   + string.punctuation
+    old_set1 = create_set(int, (1,100), 10)
+   # print(old_set1)
+    new_set1 = select_set(old_set1, int, (2,50))
+    print(new_set1)
+    print('---------------------------------------------')
+    old_set2 = create_set(float, (100, 200), 20)
+   # print(old_set2)
+    new_set2 = select_set(old_set2, float, (100, 500))
+    print(new_set2)
+    print('---------------------------------------------')
+    old_set3 = create_set(str, base_str, 50,10)
+   # print(old_set3)
+    new_set3 = select_set(old_set3, str, 'a')
+    print(new_set3)
+
 
 apply()
